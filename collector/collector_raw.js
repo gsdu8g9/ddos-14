6c8a9877062203a0ca821bd51870a400e0dbab11
Andrey <Andrey>
2016-07-08 14:56:10 +0300

    ~

--- collector/collector_raw.js
+++ collector/collector_raw.js
@@ -25,48 +25,8 @@ var config = {
 
 var PROTOCOL = { _1: "ICMP", _6: "TCP", _14: "Telnet", _17: "UDP" };
 
-/*
-//var NetFlowCollection = function () {
-//    
-//    var self = this;
-    
-//    self.flows = 0;
-//    self.bytesCount = 0;
-//    self.packetsCount = 0;
-//    self.protocols = {
-//        TCP: 0,
-//        ICMP: 0,
-//        Telnet: 0,
-//        UDP: 0,
-//    };
-    
-//    self.uniquePairs = {};
-//    self.bytesArray = [];
-    
-//    self.add = function (flow) {
-//        self.bytesCount += flow.bytesCount;
-//        self.packetsCount += flow.packetsCount;
-        
-//        self.protocols.TCP += flow.protocols.TCP;
-//        self.protocols.ICMP += flow.protocols.ICMP;
-//        self.protocols.Telnet += flow.protocols.Telnet;
-//        self.protocols.UDP += flow.protocols.UDP;
-        
-//        for (var p in flow.uniquePairs) {
-//            self.uniquePairs[p] = 1;
-//        }
 
-//    };
-    
-//    self.uniquePairsCount = 0;
-    
-//    self.inline = function (time) {
-//        self.uniquePairsCount = Object.keys(self.uniquePairs).length;
-//        return time + "," + self.uniquePairsCount + "," + self.bytesCount + "," + self.packetsCount;
-//    }
-    
-//};
-*/
+
 if (cluster.isMaster) {
     
     var benign = function () {
