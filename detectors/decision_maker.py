84dfd4e63229ed1c1d7b7e0e8d8e62f37dfc0505
Andrey <Andrey>
2016-07-21 12:38:48 +0300

    ~

--- detectors/decision_maker.py
+++ detectors/decision_maker.py
@@ -82,7 +82,7 @@ class DecisionMaker():
             print "failed", s, "!!!!!!!"
             return self.EMPTY_MALWARE_TABLE
 
-        #print "filtered, on prediction:", df[['dst', 'src']]
+        print "filtered, on prediction:", df[['dst', 'src']]
 
         pred = self.models[s].predict(df[features])
         #print pred
@@ -107,16 +107,15 @@ class DecisionMaker():
         data = self.storage.select(timestamp, depth)
         
         malware_tables_d = self.predict(data, "d")
-        print "d: ", malware_tables_d
-        
-        
+        #print "d: ", malware_tables_d
+                
         if malware_tables_d == self.EMPTY_MALWARE_TABLE:
             del data
             return result
 
 
         malware_tables_s_d = self.predict(data, "s_d", malware_tables_d)
-        print "s_d: ", malware_tables_s_d
+        #print "s_d: ", malware_tables_s_d
 
         if malware_tables_s_d == self.EMPTY_MALWARE_TABLE:
             del data
@@ -124,7 +123,7 @@ class DecisionMaker():
         
 
         malware_tables_sp_d = self.predict(data, "sp_d", malware_tables_s_d)
-        print "sp_d: ", malware_tables_sp_d
+        #print "sp_d: ", malware_tables_sp_d
 
         if malware_tables_sp_d == self.EMPTY_MALWARE_TABLE:
             del data
@@ -132,7 +131,7 @@ class DecisionMaker():
         
         
         malware_src, malware_dst = self.predict(data, "sp_dp", malware_tables_sp_d)
-        print "sp_dp: ", (malware_src, malware_dst)
+        #print "sp_dp: ", (malware_src, malware_dst)
 
         '''#!
         #s = 's_d'
